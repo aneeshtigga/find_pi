c859d7f8afcb02dbb611682cd305dc5b1849ddb8
Aneesh Tigga <aneeshtigga@gmail.com>
2020-07-17 09:36:34 +0530

    Code estimates the value of Pi

---
+++ find_pi.py
@@ -0,0 +1,13 @@
+import random
+
+def find_pi(n):
+    points_in_circle = 0
+    points_in_total = 0
+    for _ in range(n):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        distance = x**2 + y**2
+        if distance <= 1:
+            points_in_circle += 1
+        points_in_total += 1
+    return 4*points_in_circle/points_in_total
